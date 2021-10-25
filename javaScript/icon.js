5c4f21ffdb240b02f22aac8c91512d77960a7a72
Hoo <sherryho148@gmail.com>
2021-10-26 01:33:13 +0800

    change icon status if else statement

--- javaScript/icon.js
+++ javaScript/icon.js
@@ -1,55 +1,29 @@
 let get = (tag) => document.querySelector(tag)
 
-// document.body.addEventListener('click', iconAction)
-
-// export function iconAction(event) {
-//   let targetIcon = event.target
-//   let targetTask = event.target.parentNode.parentNode
-//   console.log(event.target.name)
-//   console.log('偵測到icon點擊事件', targetIcon.name)
-//   console.log('iconAction 的this是誰', this)
-//   // listedTaskArea.dispatchEvent(taskCountEvent)
-
-//   //點擊星星
-//   if (targetIcon.dataset.name === 'star') {
-//     targetIcon.classList.toggle('star_fill')
-//     clickStar(targetIcon, targetTask)
-//   }
-//   //點擊編輯
-//   if (targetIcon.dataset.name === 'pen') {
-//     targetIcon.classList.toggle('pen_fill')
-//     clickPen(targetIcon, targetTask)
-//   }
-// }
-
 // 置頂
 export function clickStar(icon, form) {
-  let listedTaskArea = get('.listed_task_area')
   let formHeader = form.firstElementChild
-  if (icon.dataset.on === 'false') {
+  icon.dataset.on = icon.dataset.on === 'true' ? 'false' : 'true'
+  console.log(icon.dataset.on)
+
+  if (icon.dataset.on === 'true') {
     console.log('開啟星星')
     icon.classList.add('star_fill')
     // 不是template 的放置頂
-    if (form.parentNode.className !== 'create_task_area') {
-      // 放到第一個task以前
-      console.log('置頂')
-      putOnTop(form)
-    }
+    putOnTop(form)
+
     // 星星變
     console.log(form.firstElementChild, 'formheader變色')
     formHeader.style.backgroundColor = '#FFF2DC'
-    icon.dataset.on = 'true'
-  } else {
+  } else if (icon.dataset.on === 'false') {
     console.log('關閉星星')
     icon.classList.remove('star_fill')
-    // let totalTasksNumber = listedTaskArea.length
-    // let lastTask = listedTaskArea.children[totalTasksNumber]
-    // listedTaskArea.insertBefore(form, lastTask)
-    putOnBottom(form)
 
     //背景回復
     formHeader.style.backgroundColor = 'initial'
-    icon.dataset.on = 'false'
+    putOnBottom(form)
+  } else {
+    return
   }
 }
 
@@ -57,7 +31,10 @@ export function clickStar(icon, form) {
 export function putOnTop(target) {
   let listedTaskArea = get('.listed_task_area')
   let firstTask = listedTaskArea.children[0]
-  listedTaskArea.insertBefore(target, firstTask)
+  target.classList.contains('template_task')
+    ? ''
+    : listedTaskArea.insertBefore(target, firstTask)
+  console.log(`${target.classList.contains('template_task')}`)
 }
 
 // 置底
@@ -65,7 +42,9 @@ export function putOnBottom(target) {
   let listedTaskArea = get('.listed_task_area')
   let totalTasksNumber = listedTaskArea.length
   let lastTask = listedTaskArea.children[totalTasksNumber]
-  listedTaskArea.insertBefore(target, lastTask)
+  target.classList.contains('template_task')
+    ? ''
+    : listedTaskArea.insertBefore(target, lastTask)
 }
 
 // 編輯
@@ -73,15 +52,18 @@ export function clickPen(icon, form) {
   let bodyAndFooter = Array.from(form.children).filter(
     (item) => item.className !== 'task_header',
   )
-  if (icon.dataset.on === 'true') {
+  icon.dataset.on = icon.dataset.on === 'true' ? 'false' : 'true'
+  if (icon.dataset.on === 'false') {
     console.log('收起來')
     icon.classList.remove('pen_fill')
     bodyAndFooter.map((section) => (section.style.display = 'none'))
     icon.dataset.on = 'false'
-  } else {
+  } else if (icon.dataset.on === 'true') {
     console.log('展開')
     icon.classList.add('pen_fill')
     bodyAndFooter.map((section) => (section.style.display = 'flex'))
     icon.dataset.on = 'true'
+  } else {
+    return
   }
 }
