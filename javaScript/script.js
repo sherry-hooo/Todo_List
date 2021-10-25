b5218eccf41def38e27b398da20b603cbd444c08
Hoo <sherryho148@gmail.com>
2021-10-25 19:10:03 +0800

    import functions

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,17 @@
-let get = (tag) => document.querySelector(tag);
+// 引入
+import { createAction } from './createTask.js'
+import { taskClickHandler } from './task.js'
+import './filterTask.js'
+import './count.js'
+
+// 取節點
+let get = (tag) => document.querySelector(tag)
+
+// 創建task事件
+let createTaskArea = get('.create_task_area')
+createTaskArea.addEventListener('click', createAction)
+
+// icon點擊事件
+let main = get('main')
+let listedTaskArea = get('.listed_task_area')
+main.addEventListener('click', taskClickHandler)
