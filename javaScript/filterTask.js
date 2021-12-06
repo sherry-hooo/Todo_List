1e41a9c0aa61bc35c9898340697d2a36b3a3a764
Hoo <sherryho148@gmail.com>
2021-12-06 21:00:06 +0800

    add toggle Menu function

--- javaScript/filterTask.js
+++ javaScript/filterTask.js
@@ -1,40 +1,64 @@
+// 引入
+import { populateList } from './populateTask.js'
+import { populateCounts } from './count.js'
+
 // 取節點
 let get = (tag) => document.querySelector(tag)
-let getAll = (tag) => document.querySelectorAll(tag)
-let createTaskButton = get('.create_task_button')
-let templateTask = get('.template_task')
-let addTaskButton = get('input.add_task_button')
-let listedTaskArea = get('.listed_task_area')
-let inputDate = get(`input[type=date]`)
-let star = get(`i[data-name=star]`)
-let pen = get(`i[data-name=pen]`)
-let showTaskCount = get('.task_count')
-let filters = get('header')
-let date = get('#date')
-let file = get('#file')
-let comment = get('#comment')
+let filteredTasks = get('.filteredTask_btn_group')
+let header = get('header')
 
-filters.addEventListener('click', filterTasks)
+// 監聽事件
+filteredTasks.addEventListener('click', filterTasks)
+header.addEventListener('click', toggleMenu)
 
-function filterTasks(event) {
-  console.log('點擊' + event.target.name + '篩選')
-  let allTask = [...listedTaskArea.children]
-  allTask.map((task) => (task.style.display = 'block'))
-  let ongoingTask = allTask.filter((form) => !form[0].checked)
-  let completedTask = allTask.filter((form) => form[0].checked)
+// toggleMenu
+function toggleMenu(event){
+  if(event.target.matches('.toggle_btn')){
+    let toggle_btn = event.target
+    toggle_btn.dataset.toggle = toggle_btn.dataset.toggle === 'false' ? true : false
+  }
+}
 
-  switch (event.target.name) {
+// filterHandler
+export function filterTasks(event) {
+  Array.from(this.children).map((button) => button.classList.remove('focus'))
+  let targetButton = event.target
+  targetButton.classList.add('focus')
+
+  let status = event.target.name
+  taskStatus(status)
+}
+
+export function taskStatus(status) {
+  console.log('篩選task!!!!!!!!')
+  let taskList = JSON.parse(localStorage.getItem('taskList')) || []
+  console.log(taskList)
+
+  switch (status) {
     case 'myTasks':
       console.log('顯示全部task')
+      //渲染
+      populateList(taskList)
+      // populateCounts(ongoingTask.length)
       break
     case 'progress':
+      let ongoingTask = taskList.filter((task) => !task.done)
+
       console.log('顯示所有進行中task')
-      completedTask.map((task) => (task.style.display = 'none'))
+      console.log(ongoingTask)
+
+      //渲染
+      populateList(ongoingTask)
+      // populateCounts(ongoingTask.length)
       break
     case 'completed':
-      console.log('顯示所有已完成task')
-      ongoingTask.map((task) => (task.style.display = 'none'))
-      showTaskCount.innerText = completedTask.length + ' task completed'
+      console.log('顯示所有已完成task', '這是完成要渲染的taskList')
+      let completedTask = taskList.filter((task) => task.done)
+      console.log(completedTask)
+
+      //渲染
+      populateList(completedTask)
+      // populateCounts(completedTask.length, 'completed')
       break
   }
 }
