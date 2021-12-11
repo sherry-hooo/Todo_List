fc7da7cd9f7b656f60c8a2f77e2df2990b0b17dd
Hoo <sherryho148@gmail.com>
2021-12-11 14:12:48 +0800

    add trash component

--- vuejs/src/views/InProgress.vue
+++ vuejs/src/views/InProgress.vue
@@ -6,25 +6,29 @@
         :key="task.id"
         :task="task"
         @editTask="editTask"
+        @dragstart="dragTask"
       ></Task>
     </section>
-  </main>
-  <footer>
-    <p>
+    <p class="displayCounts">
       {{ allProgressTasks.length }} tasks
       <span>{{ $router.name === "Completed" ? "completed" : "left" }}</span>
     </p>
+  </main>
+  <footer>
+    <Trash :draggingTaskId="draggingTaskId" @deleteTask="deleteTask"></Trash>
   </footer>
 </template>
 
 <script>
 import Task from "@/components/Task.vue";
+import Trash from "@/components/Trash.vue";
 
 export default {
-  components: { Task },
+  components: { Task, Trash },
   data() {
     return {
       allTaskData: [],
+      draggingTaskId: "",
     };
   },
   methods: {
@@ -41,6 +45,18 @@ export default {
     storageData(dataName, data) {
       // 存local
       localStorage.setItem(dataName, JSON.stringify(data));
+    },
+    dragTask(event) {
+      this.draggingTaskId = event.target.id;
+    },
+    deleteTask(taskId) {
+      this.allTaskData = JSON.parse(localStorage.getItem("taskList")) || [];
+
+      let deleteTaskIndex = this.allTaskData.findIndex(
+        (task) => task.id === Number(taskId)
+      );
+      this.allTaskData.splice(deleteTaskIndex, 1);
+      this.storageData("taskList", this.allTaskData);
     },
   },
   computed: {
